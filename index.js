f0827e3b15afb0c9191f096faf9d3a111ab9eb4c
Matheus.R <matheus.rlank2@gmail.com>
2021-09-16 17:11:08 -0300

    vs1

---
+++ index.js
@@ -0,0 +1,21 @@
+/* exercício 1 */
+
+const nome = "João"
+const salario = 1500
+const vendas = 2000
+const comissao = vendas * 15 / 100
+const total = salario + comissao
+
+
+/* exercício 2 */
+
+let info = {
+    nome : ['joão', 'Marcos','Marcelo', 'Carlos'],
+    salario: [2000, 4000, 3500, 8000],
+    vendas: [5000, 4000, 7800, 4300],
+    comissao: [5000 * 15/100, 4000 * 15/100, 7800 * 15/100, 4300 * 15/100],
+    total: [ 2000 + 750, 4000 + 600, 3500 + 1170, 8000 + 645 ]
+}
+
+console.table(info)
+
